8867ed8e2e643dad3e6137786f314c8622527f85
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2023-11-21 16:41:07 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -53,6 +54,17 @@ public class DB {
 		if (st != null) {
 			try {
 				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		
+		
+	}
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
